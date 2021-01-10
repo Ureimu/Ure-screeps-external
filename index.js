92f116e6b094d0fc9ef090e0f329e0cd1426a13d
Ureimu <57444811+Ureimu@users.noreply.github.com>
2021-01-10 14:45:57 +0800

    更新到0.03版本，已经可以运行一个测试布局任务。

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 #!/usr/bin/env node
 const { sample } = require("./lib/sample");
-const { getApi } = require('./lib/initScreepsApi')
-
-const LayoutRoomName = "E12S45"
+const { getApi,existsData } = require('./lib/initScreepsApi')
+const LayoutRoomName = "E17S44"
 const LayoutShardName = "shard3"
-
-console.log('first util');
-let api = getApi();
-if(api === ""){
+let ifExistsData = existsData();
+if (!ifExistsData) {
+    getApi();
     return;
-}else{
-    sample(LayoutRoomName,LayoutShardName,{x:25,y:25});
+} else {
+    sample(LayoutRoomName, LayoutShardName);
 }
